91636011ef737ca10a3981dcdc0723f4469a9d23
Connor Ferster <connorferster@gmail.com>
2019-05-03 11:13:03 -0700

    Additing initial files to repo

---
+++ __init__.py
@@ -0,0 +1,228 @@
+"""
+Vectortuple: Treat tuples like one dimensional vectors!
+by Connor Ferster 03/2019
+"""
+from math import pi, acos
+
+def collapse_to_tuple(d: dict, tuple_type: type) -> tuple:
+    """Returns a tuple (or namedtuple type) for the values
+    stored in the dict, 'd' as a 'tuple_type'."""
+    if tuple_type is not tuple:
+        t = tuple_type(*d.values())
+    else:
+        t = tuple(d.values())
+    return t
+
+def is_namedtuple(t: tuple) -> bool:
+    """Function written by Alex Martelli and retrieved from 
+    Stack Overflow: 
+    https://stackoverflow.com/questions/2166818/
+    how-to-check-if-an-object-is-an-instance-of-a-namedtuple
+     
+    Returns True if it appears that 't' is an instance 
+    of collections.namedtuple. Returns False otherwise.
+    """
+    tuple_type = type(t)
+    bases = tuple_type.__bases__
+    if len(bases) != 1 or bases[0] != tuple: 
+        return False
+    fields = getattr(tuple_type, '_fields', None)
+    if not isinstance(fields, tuple): 
+        return False
+    return all(type(n)==str for n in fields)
+
+def tuple_validation(t: tuple) -> None:
+    """
+    Returns True if tuple, 't', passes all validation checks.
+    Returns False otherwise.
+    """
+    all_numbers(t)
+    if not (type(t) is tuple or is_namedtuple(t)):
+        raise TypeError("Inputs tuples must be tuples: {}".format(t))
+
+def same_shape(t1: tuple, t2: tuple) -> None:
+    """
+    Returns True if t1 and t2 are the same shape. 
+    False, otherwise."""
+    if not len(t1) == len(t2):
+        raise ValueError("Tuples for vector math must be same size, not lengths {} and {}".format(len(t1), len(t2)))
+
+def all_numbers(t: tuple) -> None:
+    """
+    Returns True if all the items in, 't' are numbers.
+    False otherwise.
+    """
+    for digit in t:
+        if not isinstance(digit, (int, float)):
+            raise ValueError("Tuples for vector operations must be all numbers: {}".format(t))
+
+def tuple_check(t1: tuple, t2: tuple) -> None:
+    """Returns None. Raises error if any of the tuple validation tests fail.
+    """
+    tuple_validation(t1)
+    tuple_validation(t2)
+    same_shape(t1, t2)
+
+def dot(t1: tuple, t2: tuple) -> float:
+    """
+    Returns the dot product of the tuples, 't1' and 't2'.
+    """
+    tuple_check(t1, t2)
+    dot = 0
+    for idx, val in enumerate(t1):
+        dot += val * t2[idx]
+    return dot
+    
+def cross(t1: tuple, t2: tuple) -> tuple:
+    """
+    Returns the cross product of two 3-dimensional tuples, 't1' and 't2'.
+    (Raises error if tuples are not 3-dimensional). Maintains the tuple type
+    't1' (e.g. if it is a namedtuple).
+    """
+    tuple_check(t1, t2)
+    if len(t1) == len(t2) == 3:
+        i = t1[1]*t2[2] - t2[1]*t1[2]
+        j = -(t1[0]*t2[2] - t2[0]*t1[2])
+        k = t1[0]*t2[1] - t2[0]*t1[1]
+        if type(t1) is tuple:
+            return (i,j,k)
+        else:
+            return type(t1)(i,j,k)
+    else:
+        raise TypeError("Input tuples must be 3-dimensional. Got: {t1}, {t2}".format(t1=t1, t2=t2))
+        
+def add(t1: tuple, other) -> tuple:
+    """
+    Returns a tuple of element-wise multiplication of 't1' and 'other'
+    """
+    tuple_validation(t1)
+    if isinstance(other, (int, float)):
+        out_dict = {idx: val+other for idx, val in enumerate(t1)}
+    else: 
+        tuple_check(t1, other)
+        out_dict = {idx: val+other[idx] for idx, val in enumerate(t1)}
+    return collapse_to_tuple(out_dict, type(t1))
+
+def subtract(t1: tuple, other) -> tuple:
+    """
+    Returns a tuple of element-wise multiplication of 't1' and other
+    """
+    tuple_validation(t1)
+    if isinstance(other, (int, float)):
+        out_dict = {idx: val-other for idx, val in enumerate(t1)}
+    else: 
+        tuple_check(t1, other)
+        out_dict = {idx: val-other[idx] for idx, val in enumerate(t1)}
+    return collapse_to_tuple(out_dict, type(t1))
+        
+def multiply(t1: tuple, other) -> tuple:
+    """
+    Returns a tuple of element-wise multiplication of 't1' and other
+    """
+    tuple_validation(t1)
+    if isinstance(other,(int, float)):
+        out_dict = {idx: val*other for idx, val in enumerate(t1)}
+    else: 
+        tuple_check(t1, other)
+        out_dict = {idx: val*other[idx] for idx, val in enumerate(t1)}
+    return collapse_to_tuple(out_dict, type(t1))
+
+def divide(t1: tuple, other) -> tuple:
+    """
+    Returns a tuple of element-wise division of 't1' and 't2'
+    """
+    out_dict = {}
+    if isinstance(other, (int, float)):
+        tuple_validation(t1)
+        for idx, val in enumerate(t1):
+            if val == 0 and other == 0:
+                out_dict.update({idx: float("nan")})
+            elif other == 0:
+                out_dict.update({idx: float("inf")})
+            else:
+                out_dict.update({idx: val/other})
+    else: 
+        tuple_check(t1, other)
+        for idx, val in enumerate(t1):
+            if val == 0 and other[idx] == 0:
+                out_dict.update({idx: float("nan")})
+            elif other[idx] == 0:
+                out_dict.update({idx: float("inf")})
+            else:
+                out_dict.update({idx: val/other[idx]})
+    return collapse_to_tuple(out_dict, type(t1))
+
+def vround(t: tuple, precision = 0) -> tuple:
+    """
+    Returns a tuple with elements rounded to 'precision'.
+    """
+    tuple_validation(t)
+    out_dict = {idx: round(val, precision) for idx, val in enumerate(t)}
+    return collapse_to_tuple(out_dict, type(t))
+
+def mean(t: tuple, ignore_empty = False) -> float:
+    """
+    Returns the average of the values in the tuple, 't'. If 'ignore' is True,
+    then only the values that are not either 0 or None are averaged.
+    If 'ignore' is False, all values are used with None taken as 0. 
+    """
+    tuple_validation(t)
+    count = 0
+    total = 0
+    for val in t:
+        if ignore_empty: 
+            if val == 0 or val == None:
+                continue
+            else:
+                total += val
+                count += 1
+        else:
+            total += val
+            count += 1  
+    return total/count
+
+def magnitude(t: tuple) -> float:
+    """
+    Returns the magnitude of the tuple, 't', as a vector though it were
+    a vector.
+    """
+    tuple_validation(t)
+    mag2 = 0
+    for val in t:
+        mag2 += val**2
+    return mag2**(1/2)
+    
+def normalize(t: tuple) -> tuple:
+    """
+    Returns the normalized unit vector of the vector tuple, 't'.
+    """
+    tuple_validation(t)
+    return divide(t, magnitude(t))
+
+def _clip(n: float) -> float:
+    """
+    Helper function to emulate numpy.clip for the specific
+    use case of preventing math domain errors on the 
+    acos function. 
+    """
+    sign = n / abs(n)
+    if abs(n) > 1: return 1 * sign
+    else: return n
+        
+
+def angle(t1: tuple, t2: tuple, degrees = False) -> float:
+    """
+    Returns the angle between two vector tuples, 't1' and 't2',
+    in rads. If 'degrees' = True, then returned in degrees.
+    """
+    # TODO: Fix math domain error when doing acos(1) for parallel vectors
+    rad2deg = 1
+    if degrees:
+        rad2deg = 180/pi
+    tuple_check(t1,t2)
+    denom = (magnitude(t1) * magnitude(t2))
+    
+    if denom == 0:
+        return pi/4 * rad2deg
+    else:
+        return acos(_clip(dot(t1, t2) / (magnitude(t1) * magnitude(t2)))) * rad2deg
---
+++ test_tuplevector.py
@@ -0,0 +1,124 @@
+import math
+from collections import namedtuple
+import tuplevector as vec
+import numpy as np
+import pytest as pytest
+
+
+Point = namedtuple("Point", ["x", "y", "z"])
+Dimensions = namedtuple("Dimensions", ["kg", "m", "s", "A", "cd", "K", "mol"])
+
+P1 = Point(2.0, 3.4, 1.0)
+P2 = Point(1.5, 2.8, 4.5)
+P3 = Point(1, 1, 0)
+P4 = Point(1, 0, 0)
+P5 = Point(-1, 0, 0)
+P6 = Point(3, 4, 0)
+P7 = Point(0,0,0)
+D1 = Dimensions(1., 1., -2., 0,0,0,0)
+D2 = Dimensions(2., 2., -4., 0,0,0,0)
+D3 = Dimensions(1., 2., -2., 0,0,0,0)
+D4 = Dimensions(-1, 1, 0, 0, 0, 0, 0)
+T1 = (4.5, 1.1112, 2)
+T2 = (1.2, 4, 3.267)
+T3 = (3, 4, 0)
+T4 = (0,0,0)
+T5 = (1/2, math.sqrt(3)/2, 0)
+T6 = (1/2, 0, 0)
+NT1 = ("A", 2, 3.4)
+NT2 = ("B", "C", "D")
+L1 = tuple(range(0,100))
+L2 = tuple(range(1,101))
+LT1 = list(range(0,100))
+LT2 = list(range(1, 101))
+
+def test_collapse_to_tuple():
+    P1A = vec.collapse_to_tuple(dict(enumerate(P1)), type(P1))
+    assert P1 == P1A
+    D1A = vec.collapse_to_tuple(dict(enumerate(D1)), type(D1))
+    assert D1 == D1A
+    T1A = vec.collapse_to_tuple(dict(enumerate(T1)), type(T1))
+    assert T1 == T1A
+    
+def test_isnamedtuple():
+    assert vec.is_namedtuple(P1) == True
+    assert vec.is_namedtuple(T1) == False
+
+def test_tuple_validation():
+    with pytest.raises(ValueError):
+        vec.tuple_validation(NT1)
+        vec.tuple_validation(NT2)
+    with pytest.raises(TypeError):
+        vec.tuple_validation(LT1)
+        vec.tuple_validation(LT2)
+    assert vec.tuple_validation(P1) is None
+    assert vec.tuple_validation(T1) is None
+    assert vec.tuple_validation(L1) is None
+    
+def test_same_shape():
+    with pytest.raises(ValueError):
+        vec.same_shape(P1, D1)
+        vec.same_shape(T2, L1)
+    assert vec.same_shape(NT1, NT2) is None
+    assert vec.same_shape(T1, P3) is None
+    
+def test_dot():
+    assert vec.dot(P1, P3) == 5.4
+    assert vec.dot(D1, D3) == 7.0
+    assert vec.dot(T1, T2) == 16.3788
+    
+def test_cross():
+    assert vec.cross(P1, P3) == Point(x=-1.0, y=1.0, z=-1.4)
+    assert vec.cross(T1, T2) == (-4.3697096, -12.301499999999999, 16.66656)
+    
+def test_add():
+    assert vec.add(P1, P3) == Point(x=3.0, y=4.4, z=1.0)
+    assert vec.add(D2, D3) == Dimensions(kg=3.0, m=4.0, s=-6.0, A=0.0, cd=0.0, K=0.0, mol=0.0)
+    assert vec.add(L1, L2) == tuple(range(1, 200, 2))
+    
+def test_subtract():
+    assert vec.subtract(P1, P3) == Point(x=1.0, y=2.4, z=1.0)
+    assert vec.subtract(D2, D3) == Dimensions(kg=1.0, m=0.0, s=-2.0, A=0.0, cd=0.0, K=0.0, mol=0.0)
+    assert vec.subtract(L1, L2) == tuple(-1 for n in range(0,100))
+    
+def test_multiply():
+    assert vec.multiply(P1, P3) == Point(x=2.0, y=3.4, z=0.0)
+    assert vec.multiply(D2, D3) == Dimensions(kg=2.0, m=4.0, s=8.0, A=0.0, cd=0.0, K=0.0, mol=0.0)
+    assert vec.multiply(P3, 3.5) == Point(x=3.5, y=3.5, z=0)
+    
+def test_divide():
+    assert vec.divide(P1, P3) == Point(x=2.0, y=3.4, z=float("inf"))
+    assert vec.divide(D2, 2) == D1
+    
+def test_magnitude():
+    assert vec.magnitude(P6) == 5.0
+    assert vec.magnitude(P7) == 0
+    assert vec.magnitude(T3) == 5.0
+    assert vec.magnitude(P3) == math.sqrt(2)
+    assert vec.magnitude(D4) == math.sqrt(2)
+    
+def test_vround():
+    assert vec.vround(T1, 3) == (4.5, 1.111, 2)
+    assert vec.vround(P2, 0) == Point(2, 3, 4)
+    
+def test_mean():
+    assert vec.mean(P6) == 2.3333333333333335
+    assert vec.mean(D1) == 0
+    assert vec.mean(T2) == 2.8223333333333334
+    assert vec.mean(P6, True) == 3.5
+    assert vec.mean(L1) == 49.5
+    
+def test_normalize():
+    assert vec.normalize(P1) == Point(x=0.49147318718299043, y=0.8355044182110838, z=0.24573659359149522)
+    assert vec.normalize(T1) == (0.8913991044053705, 0.22011615218116617, 0.39617737973572026)
+    assert vec.normalize(D3) == Dimensions(kg=0.3333333333333333, m=0.6666666666666666, s=-0.6666666666666666, A=0.0, cd=0.0, K=0.0, mol=0.0)
+    
+def test_angle():
+    assert vec.angle(P3, P4) == 0.7853981633974484
+    assert vec.angle(P3, P4, True) == 45.00000000000001
+    assert vec.angle(T5, T6) == 1.0471975511965976
+    assert vec.angle(T5, T6, True) == 59.99999999999999
+    
+
+    
+    
